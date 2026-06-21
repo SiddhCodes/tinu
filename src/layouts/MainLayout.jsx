d361cf78ac928851c965537da4ab75c607d1bdbc
Siddhant Mul <siddhantmul18@gmail.com>
2026-06-21 08:21:21 +0530

    Initial project setup

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import { Footer } from "../shared/Footer";
+import { Header } from "../shared/Header";
+
+export const MainLayout = () => {
+  return (
+    <div>
+      <Header />
+      <main></main>
+      <Footer />
+    </div>
+  );
+};
